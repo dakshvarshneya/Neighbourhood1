e679c10b9978f03b8651e80b59aa625d09c22a22
dakshvarshneya <dakshvarshneya5@gmail.com>
2017-08-30 00:20:20 +0530

    initial commit

---
+++ js/locations.js
@@ -0,0 +1,62 @@
+var myLocations = [
+    {
+        title: 'Gandhi Smriti',
+        lat: 28.601780,
+        lng: 77.214339
+    },
+    {
+        title: 'Rashtrapati Bhawan',
+        lat: 28.614360,
+        lng: 77.199621
+    },
+    {
+        title: 'Lord of the drinks',
+        lat: 28.631849,
+        lng: 77.216913
+    },
+    {
+        title: 'Taj Palace',
+        lat: 28.595453,
+        lng: 77.171027
+
+    },
+    {
+        title: 'The Leela Palace',
+        lat: 28.580038,
+        lng: 77.189038
+
+    },
+    {
+        title: 'Adventure Island',
+        lat: 28.723728,
+        lng: 77.113592
+
+    },
+    {
+        title: 'Akshardham',
+        lat: 28.612673,
+        lng: 77.277262
+
+    },
+    {
+        title: 'Dlf Emporio',
+        lat: 28.543425,
+        lng: 77.156765
+    },
+    {
+        title: 'Red Fort',
+        lat: 28.656159,
+        lng: 77.24102
+    },
+    {
+        title: 'Jantar Mantar',
+        lat: 28.627055,
+        lng: 77.216627
+    },
+    {
+        title: 'Lotus Temple',
+        lat: 28.553492,
+        lng: 77.258826
+    }
+
+]
